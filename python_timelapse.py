9fb5d55efe6424561a621b4171302ade4bbbf540
Martin Ashby <martin@ashbysoft.com>
2016-09-18 21:17:10 +0000

    Changed timelapse script so it's executed from cron now instead of sleeping

--- python_timelapse.py
+++ python_timelapse.py
@@ -1,31 +1,28 @@
 import os
+import re
 import time
 import python_camera_client
-import itertools
 
-OUTPUT_DIR = "timelapse"
-INTERVAL_SECONDS = 300
+OUTPUT_DIR = "/home/pi/camera_scripts/timelapse"
 
+def get_file_num(file):
+    num_str = re.match(r'image(\d+).jpg', file).group(1)
+    return int(num_str)
 
-def start_timelapse():
+def do_timelapse():
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
-
     existing_files = os.listdir(OUTPUT_DIR)
-    existing_files.sort()
-    last_file = existing_files[-1]
-    last_num_str = re.match(r'image(\d+).jpg').group(1)
-    start_number = int(last_num_str)
+    numbers = [get_file_num(x) for x in existing_files]
+    start_number = max(numbers) + 1
 
-    for i in itertools.count(start=0, step=1):
-        image_data = python_camera_client.capture_image()
-        with open(OUTPUT_DIR + "/image{}.jpg".format(i), "wb") as f:
-            f.write(image_data)
-        time.sleep(INTERVAL_SECONDS)
+    image_data = python_camera_client.capture_image()
+    with open(OUTPUT_DIR + "/image{}.jpg".format(start_number), "wb") as f:
+        f.write(image_data)
         
 
 if __name__=="__main__":
-    start_timelapse()
+    do_timelapse()
 
 
